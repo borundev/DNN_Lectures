f86a5174d79f2e5e8046aceaa9b2ca92329878c9
borundev <borundev@gmail.com>
2019-12-04 14:03:30 +0100

    Added bias weight updates

--- Layer.py
+++ Layer.py
@@ -1,4 +1,6 @@
 import numpy as np
+
+
 class Layer(object):
 
     def __init__(self, weights, trainable=True, learning_rate=0.001, name=None, first_layer=False):
@@ -16,7 +18,6 @@ class Layer(object):
     def on_first_feed_forward(self):
         pass
 
-
     def back_prop(self, next_layer_loss_gradient):
         raise NotImplementedError
 
@@ -25,17 +26,22 @@ class Layer(object):
             np.add(self.weights,
                    -self.learning_rate * self.loss_derivative_weights,
                    out=self.weights)
+            if hasattr(self, 'bias'):
+                np.add(self.bias,
+                       -self.learning_rate * self.loss_drivative_bias,
+                       out=self.bias
+                       )
 
 
 class ActivationFunction(Layer):
 
-    def __init__(self,activation_function):
-        self.activation_function=activation_function
-        self.trainable=False
+    def __init__(self, activation_function):
+        self.activation_function = activation_function
+        self.trainable = False
 
     def feed_forward(self, prev_layer):
-        self.prev_layer=prev_layer
+        self.prev_layer = prev_layer
         return self.activation_function(prev_layer)
 
     def back_prop(self, next_layer_loss_gradient):
-        return next_layer_loss_gradient*self.activation_function(self.prev_layer,der=True)
+        return next_layer_loss_gradient * self.activation_function(self.prev_layer, der=True)
